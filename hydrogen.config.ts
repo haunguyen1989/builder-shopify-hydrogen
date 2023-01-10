6d5498ce92aa50fb26855f6bb68267f96e177854
Hau Nguyen <Hau.Nguyen@isobar.com>
2023-01-10 12:24:10 +0700

    init package

---
+++ hydrogen.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    defaultCountryCode: 'US',
+    defaultLanguageCode: 'EN',
+    storeDomain: 'merkle-vn.myshopify.com',
+    storefrontToken: '85e4ab6d03bcea5e182b1f327b04b122',
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: import.meta.env.PROD,
+    sameSite: 'Strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+});
