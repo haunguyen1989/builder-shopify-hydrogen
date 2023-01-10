75443a16f834595c2afc870b302cc58dc6b25ec7
Hau Nguyen <Hau.Nguyen@isobar.com>
2023-01-10 16:36:00 +0700

    set custom component2

--- src/routes/components/BuilderComponent.client.tsx
+++ src/routes/components/BuilderComponent.client.tsx
@@ -1,8 +1,6 @@
 // Do stuff here
 import {BuilderComponent, builder, Builder} from '@builder.io/react';
-const Heading = props => (
-    <h1>{props.title}</h1>
-)
+
 builder.init('8141715a0ca14a24a7ec39204b570b0e');
 
 // Add custom components
@@ -10,10 +8,6 @@ builder.init('8141715a0ca14a24a7ec39204b570b0e');
 // Builder.registerComponent(Welcome, {
 //   name: 'Welcome',
 // });
-Builder.registerComponent(Heading, {
-    name: 'Heading',
-    inputs: [{ name: 'title', type: 'text' }],
-    image: 'https://tabler-icons.io/static/tabler-icons/icons-png/3d-cube-sphere-off.png'
-})
+
 
 export default BuilderComponent;
